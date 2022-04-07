31512cc99b575ac86abeda655386e39594b5090a
Akogwu Emmanuel <akogwuemmanuel57@gmail.com>
2022-04-07 00:17:22 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("*** Hello World, welcome to Jenkins Training. Some changes ****");
+        System.out.println("*** Triger using webhook ****");
     }
 }
